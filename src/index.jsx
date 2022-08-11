fa82bde3ebef0b7068b2eb61174ef4341c64caf6
Dmitry Kalalb <gdydhd0134h@gmail.com>
2022-08-11 22:54:36 +0200

    app init

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createGlobalStyle} from 'styled-components'
+
+const Global = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Global />
     <App />
   </React.StrictMode>
 );
